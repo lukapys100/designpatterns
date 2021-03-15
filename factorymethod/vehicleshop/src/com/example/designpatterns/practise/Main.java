367906bee147ef38c9c593ebbefb30f0310b2bc2
lukapys100 <l.pyszny1902@hotmail.com>
2021-03-15 14:56:35 +0100

    First project commit

---
+++ factorymethod/vehicleshop/src/com/example/designpatterns/practise/Main.java
@@ -0,0 +1,8 @@
+package com.example.designpatterns.practise;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Lets see whether project works.");
+    }
+}
