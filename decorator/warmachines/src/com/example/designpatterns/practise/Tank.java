908e017a4c0b6ad32685ad5f9d68f185fa1761a9
lukapys100 <l.pyszny1902@hotmail.com>
2021-03-23 23:05:20 +0100

    Implementing Tank(plain class)

--- decorator/warmachines/src/com/example/designpatterns/practise/Tank.java
+++ decorator/warmachines/src/com/example/designpatterns/practise/Tank.java
@@ -2,5 +2,44 @@ package com.example.designpatterns.practise;
 
 
 //Specified plain class
-public class Tank {
+public class Tank implements WarMachine{
+    private boolean isArmed = false;
+    private int currentCrosshairPositionX;
+    private int currentCrosshairPositionY;
+    private final int TANK_ACCURACY_OVERSHOOT = 10;
+
+    public Tank() {
+    }
+
+    @Override
+    public void arm() {
+        this.isArmed = true;
+        System.out.println("Tank is ready to shoot now!");
+    }
+
+    @Override
+    public void aim(int x, int y) {
+        if(x>=0 && y>=0){
+            currentCrosshairPositionX = x+100;
+            currentCrosshairPositionY = y+100;
+            while(currentCrosshairPositionX >= x+TANK_ACCURACY_OVERSHOOT
+                    && currentCrosshairPositionY >= y+TANK_ACCURACY_OVERSHOOT){
+                currentCrosshairPositionX -= 1;
+                currentCrosshairPositionY -= 1;
+            }
+        }
+    }
+
+    @Override
+    public void shoot() {
+        if(isArmed){
+            System.out.println("SHOT!");
+            reload();
+        }
+    }
+
+    @Override
+    public void reload() {
+        System.out.println("Reload...");
+    }
 }
